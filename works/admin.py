758b2c8744022f3bc0489b027c980654aa2da073
jasur05 <jasurisrailov1@gmail.com>
2022-06-29 16:09:03 +0500

    added works model

--- works/admin.py
+++ works/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import WorksCategoryModel, WorksModel
 
-# Register your models here.
+@admin.register(WorksCategoryModel)
+class WorksCategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_link = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(WorksModel)
+class WorksModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at']
+    list_display_links = ['id', 'title', 'created_at']
+    search_fields = ['title']
--- works/models.py
+++ works/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+
+class WorksCategoryModel(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'work'
+        verbose_name_plural = 'works'
+
+class WorksModel(models.Model):
+    title = models.CharField(max_length=255)
+    body = RichTextUploadingField
+    created_at = models.DateTimeField(auto_now_add=True)
+    work_image = models.ImageField(upload_to='works')
+    category = models.ManyToManyField(WorksCategoryModel)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'work'
+        verbose_name_plural = 'works'
